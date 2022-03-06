9d3698c0305fcf11b09fbdb6d24cfd4ac13e3d93
gera0220 <a329611@uach.mx>
2022-03-06 15:05:46 -0700

    Conversión de archivos wav a features

--- src/codigo_base.py
+++ src/codigo_base.py
@@ -2,42 +2,49 @@ from scipy.io.wavfile import read
 import numpy as np
 from python_speech_features.base import mfcc
 import matplotlib.pyplot as plt
+import os
 
-fs, audio = read('prueba.wav')
+os.chdir('../data/raw')
+audio_files = os.listdir()
+print(audio_files)
 
-print(f'Frecuencia de muestreo: {fs}')
-print(f'Dimensiones del audio: {audio.shape[0]} puntos')
+for file in audio_files:
 
-audio = audio[0:fs]
+    fs, audio = read(file)
 
-# Si graficamos el audio tenemos el siguiente diagrama.
-plt.figure(1, figsize = (15, 5))
-plt.plot(audio, color = "black")
-plt.grid()
-plt.xlabel("Muestras")
-plt.ylabel("Amplitud")
-plt.show()
+    print(f'Frecuencia de muestreo: {fs}')
+    print(f'Dimensiones del audio: {audio.shape[0]} puntos')
 
-# Vamos a obtener sus coeficientes.
-# Parámetros que recibe:
-#   - signal: la señal de audio.
-#   - winlen: tamaño de la ventana en segundos.
-#   - winstep: tamaño del avance (se utiliza para el prcentaje de muestras que habrá entre ventanas). En este caso se tiene un overlap de 50%.
-#   - numcep: número de coeficientes de Mel.
-#   - nfilt: número de filtros del banco.
-coeffs = mfcc(signal = audio, samplerate = fs, winlen = 0.030, winstep = 0.015, numcep = 25, nfilt = 30)
+    audio = audio[0:fs]
 
-# Vamos a verlos en un gráfico
-plt.figure(2, figsize=(15,4))
-plt.imshow(coeffs.T, cmap="jet")
-plt.show()
+    # Si graficamos el audio tenemos el siguiente diagrama.
+    #plt.figure(1, figsize = (15, 5))
+    #plt.plot(audio, color = "black")
+    #plt.grid()
+    #plt.xlabel("Muestras")
+    #plt.ylabel("Amplitud")
+    #plt.show()
 
-# A esta matriz hay que pegarle la clase a la que corresponde, como una columna más, esto es muy fácil ya que simplemente generamos un vector con el mísmo número (en este caso le estoy pegando puros ceros)
+    # Vamos a obtener sus coeficientes.
+    # Parámetros que recibe:
+    #   - signal: la señal de audio.
+    #   - winlen: tamaño de la ventana en segundos.
+    #   - winstep: tamaño del avance (se utiliza para el prcentaje de muestras que habrá entre ventanas). En este caso se tiene un overlap de 50%.
+    #   - numcep: número de coeficientes de Mel.
+    #   - nfilt: número de filtros del banco.
+    coeffs = mfcc(signal = audio, samplerate = fs, winlen = 0.030, winstep = 0.015, numcep = 25, nfilt = 30, nfft = 1500)
 
-classes = np.matrix([0]*coeffs.shape[0])
+    # Vamos a verlos en un gráfico
+    #plt.figure(2, figsize=(15,4))
+    #plt.imshow(coeffs.T, cmap="jet")
+    #plt.show()
 
-# Lo pegamos a la matriz.
-features = np.hstack([coeffs, classes.T])
+    # A esta matriz hay que pegarle la clase a la que corresponde, como una columna más, esto es muy fácil ya que simplemente generamos un vector con el mísmo número (en este caso le estoy pegando puros ceros)
 
-print(features.shape) # este archivo ya puede guardarse en la computadora.
-np.savetxt("mfcc_guitar.txt", features, delimiter=",", fmt="%f")
+    classes = np.matrix([0]*coeffs.shape[0])
+
+    # Lo pegamos a la matriz.
+    features = np.hstack([coeffs, classes.T])
+
+    print(features.shape) # este archivo ya puede guardarse en la computadora.
+    np.savetxt('../features/mfcc_' + file.split('.')[0] + '.txt', features, delimiter=",", fmt="%f")
